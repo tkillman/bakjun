e0545c4bd558f20d31036560b990a6d075b334f7
kimdonggyu <dkkim1004@hanmail.net>
2024-08-27 22:56:52 +0900

    q_2563

--- index.js
+++ index.js
@@ -1,26 +1,46 @@
 const fs = require('fs');
 const input = fs.readFileSync('input.txt').toString().trim().split("\n");
 
-const strArr = [];
-let answer = [];
+const board = [];
 
-const solution = () => {
+const createBoard = ()=>{
 
-  for (let i = 0; i < input.length; i++) {
-    const row = input[i].split("");
-    strArr.push(row);
+  for (let i = 0; i < 100; i++) {
+    const row = [];
+    for (let j = 0; j < 100; j++) {
+      row.push(0);
+    }
+    board.push(row);
   }
 
-  for (let i = 0; i < 15; i++) {
-    for (let j = 0; j < 5; j++) {
-      const str = strArr[j][i]?.trim();
-      if (str !== undefined) {
-        //console.log("i",i ,"j",j, "str",str);
-        answer.push(String(str));
+}
+
+const paintBoard = (startXIndex, startYIndex) => {
+  for (let i = startXIndex; i < (startXIndex + 10); i++) {
+    const row = board[i];
+    for (let j = startYIndex; j < (startYIndex + 10); j++) {
+      row[j] = 1;
+    }
+  }
+}
+
+const solution = () => {
+  createBoard();
+
+  for (let i = 0; i < input.length; i++) {
+    const [startX, startY] = input[i].split(" ").map(Number);
+    paintBoard(startX, startY);
+  }
+
+  let count = 0;
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+      if (board[i][j] === 1) {
+        count++;
       }
     }
   }
-  console.log(answer.join(""));
+  console.log(count);
 }  
 
 solution();
