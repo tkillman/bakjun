1ec3b56e5d29fadad9ae36b64d3fddee23949ef1
kimdonggyu <dkkim1004@hanmail.net>
2024-08-31 13:01:36 +0900

    q_2745

--- index.js
+++ index.js
@@ -1,46 +1,97 @@
-const fs = require('fs');
-const input = fs.readFileSync('input.txt').toString().trim().split("\n");
+const fs = require("fs");
+const [value, jin] = fs.readFileSync("input.txt").toString().trim().split(" ");
 
-const board = [];
-
-const createBoard = ()=>{
-
-  for (let i = 0; i < 100; i++) {
-    const row = [];
-    for (let j = 0; j < 100; j++) {
-      row.push(0);
-    }
-    board.push(row);
+const switchNum = (char) => {
+  switch (char) {
+    case "0":
+      return 0;
+    case "1":
+      return 1;
+    case "2":
+      return 2;
+    case "3":
+      return 3;
+    case "4":
+      return 4;
+    case "5":
+      return 5;
+    case "6":
+      return 6;
+    case "7":
+      return 7;
+    case "8":
+      return 8;
+    case "9":
+      return 9;
+    case "A":
+      return 10;
+    case "B":
+      return 11;
+    case "C":
+      return 12;
+    case "D":
+      return 13;
+    case "E":
+      return 14;
+    case "F":
+      return 15;
+    case "G":
+      return 16;
+    case "H":
+      return 17;
+    case "I":
+      return 18;
+    case "J":
+      return 19;
+    case "K":
+      return 20;
+    case "L":
+      return 21;
+    case "M":
+      return 22;
+    case "N":
+      return 23;
+    case "O":
+      return 24;
+    case "P":
+      return 25;
+    case "Q":
+      return 26;
+    case "R":
+      return 27;
+    case "S":
+      return 28;
+    case "T":
+      return 29;
+    case "U":
+      return 30;
+    case "V":
+      return 31;
+    case "W":
+      return 32;
+    case "X":
+      return 33;
+    case "Y":
+      return 34;
+    case "Z":
+      return 35;
+    default:
+      break;
   }
-
-}
-
-const paintBoard = (startXIndex, startYIndex) => {
-  for (let i = startXIndex; i < (startXIndex + 10); i++) {
-    const row = board[i];
-    for (let j = startYIndex; j < (startYIndex + 10); j++) {
-      row[j] = 1;
-    }
-  }
-}
+};
 
 const solution = () => {
-  createBoard();
+  const valueArr = value.split("").reverse();
 
-  for (let i = 0; i < input.length; i++) {
-    const [startX, startY] = input[i].split(" ").map(Number);
-    paintBoard(startX, startY);
-  }
+  let answer = 0;
+  for (let i = 0; i < valueArr.length; i++) {
+    const targetNum = switchNum(valueArr[i]);
 
-  let count = 0;
-  for (let i = 0; i < 100; i++) {
-    for (let j = 0; j < 100; j++) {
-      if (board[i][j] === 1) {
-        count++;
-      }
-    }
+    const result = targetNum * Math.pow(Number(jin), i);
+
+    answer += result;
   }
-  console.log(count);
-}  
+  console.log(answer);
+};
 
 solution();
