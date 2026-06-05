990eb08a82b58bc2e43314164c873081054de7f0
김동규 <tkillman@logiall.com>
2026-06-05 13:29:48 +0900

    q_181941

---
+++ question/q_181941.js
@@ -0,0 +1,11 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181941
+
+function solution(arr) {
+  var answer = '';
+  answer = arr.join('');
+  return answer;
+}
+
+const arr = ['a', 'b', 'c'];
+const result = solution(arr);
+console.log(result);
