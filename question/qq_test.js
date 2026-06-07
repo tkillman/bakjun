0e09f5e1a5cd75cf619317f43aaf490b606cf3ff
kimdonggyu <dkkim1004@hanmail.net>
2026-06-07 23:29:32 +0900

    qq_test

---
+++ question/qq_test.js
@@ -0,0 +1,24 @@
+const printFn = (num) => {
+  const str = new Promise((resolve) => {
+    setTimeout(() => {
+      console.log("num :: ", num);
+      resolve(num);
+    }, 0);
+  });
+
+  return str;
+};
+
+const solution = () => {
+  Array(2)
+    .fill()
+    .forEach(async (_, index) => {
+      console.log("start index :: ", index);
+      const num = await printFn(index);
+      console.log("end num :: ", num);
+    });
+
+  console.log("finish");
+};
+
+solution();
